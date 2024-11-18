08eb7e575e06128ab6cfd4239a6fce956d720a57
jfsc27 <juanf.smithc@uqvirtual.edu.co>
2024-11-18 07:43:53 -0500

    Commit

---
+++ ProyectoFinalVersionFinal/src/main/java/co/edu/uniquindio/projectfinal/proyectofinalversionfinal/viewController/RegistroViewController.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.projectfinal.proyectofinalversionfinal.viewController;
+
+import co.edu.uniquindio.projectfinal.proyectofinalversionfinal.controller.UsuarioController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistroViewController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
